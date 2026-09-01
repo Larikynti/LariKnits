85e2f8adff7e5a2151d73238f8fe95099932c97e
Larikynti <lariknits@gmail.com>
2026-09-01 14:44:23 +0200

    Add Scrunchie calculator configuration
    Added configuration settings for the Scrunchie calculator, including name, default hair tie circumference, and available sizes.

--- Calculators/Scrunchie/config.js
+++ Calculators/Scrunchie/config.js
@@ -1,1 +1,29 @@
+const scrunchieConfig = {
 
+    // Calculator name
+    name: "Scrunchie",
+
+    // Hair tie default circumference
+    defaultHairTieCircumference: 5,
+
+    // Available sizes
+    sizes: {
+
+        S: {
+            length: 50,
+            width: 5
+        },
+
+        M: {
+            length: 70,
+            width: 8
+        },
+
+        L: {
+            length: 90,
+            width: 12
+        }
+
+    }
+
+};
